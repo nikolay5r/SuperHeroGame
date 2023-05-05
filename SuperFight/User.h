fdfc38a5aacd98a619d7fcd3ab97b61269a4fa08
nikolay5r <nradilov30@gmail.com>
2023-05-05 19:52:45 +0300

    Added function declarations for User class

--- SuperFight/User.h
+++ SuperFight/User.h
@@ -3,10 +3,29 @@
 
 class User
 {
-	MyString firstName;
-	MyString secondName;
-	MyString email;
-	MyString password;
+protected:
+	MyString firstName = "";
+	MyString secondName = "";
+	MyString userName = "";
+	MyString email = "";
+	MyString password = "";
 
+	MyString fullName = "";
+
+	void setFirstName(MyString firstName);
+	void setLastName(MyString fullName);
+	void setEmail(MyString email);
+	void setPassword(MyString password);
+	void setUsername(MyString username);
+	void setFullName();
+public:
+	User(MyString firstName, MyString secondName, MyString username, MyString email, MyString password);
+
+	const MyString& getPassword() const;
+	const MyString& getEmail() const;
+	const MyString& getUserName() const;
+	const MyString& getFullName() const;
+	const MyString& getFirstName() const;
+	const MyString& getLastName() const;
 };
 
