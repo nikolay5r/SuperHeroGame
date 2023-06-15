5daf4ca93e6d9e0341f58f664f0467b8baebe6ef
nikolay5r <nradilov30@gmail.com>
2023-06-15 12:58:53 +0300

    Moved getFileSize function and find entity through username file

--- SuperFight/HelperFunctions.cpp
+++ SuperFight/HelperFunctions.cpp
@@ -1,5 +1,6 @@
 #include "HelperFunctions.h"
 #include "Regex_Error.h"
+#include <fstream>
 
 bool helper::isUpper(char a)
 {
@@ -14,4 +15,46 @@ bool helper::isLower(char a)
 bool helper::isDigit(char a)
 {
 	return a >= '0' && a <= '9';
+}
+
+size_t helper::getFileSize(std::ifstream& file)
+{
+	size_t curr = file.tellg();
+	file.seekg(0, std::ios::end);
+	size_t size = file.tellg();
+	file.seekg(curr);
+
+	return size;
+}
+
+int helper::findEntityIndexInFile(const MyString& filePath, const MyString& usernameToFind)
+{
+	std::ifstream file(filePath.c_str(), std::ios::binary);
+
+	int indexInPlayersFile = 0;
+	while (!file.eof())
+	{
+		size_t length = 0;
+		file.read((char*)&length, sizeof(length));
+		char* username = new char[length + 1];
+		file.read(username, length + 1);
+		file.read((char*)&indexInPlayersFile, sizeof(indexInPlayersFile));
+
+		if (usernameToFind == username)
+		{
+			file.close();
+			delete[] username;
+			return indexInPlayersFile;
+		}
+
+		delete[] username;
+
+		if (file.eof())
+		{
+			break;
+		}
+	}
+
+	file.close();
+	return -1;
 }
--- SuperFight/HelperFunctions.h
+++ SuperFight/HelperFunctions.h
@@ -1,8 +1,11 @@
 #pragma once
 #include "MyString.h"
+#include <fstream>
 
 namespace helper {
 	bool isUpper(char a);
 	bool isLower(char a);
 	bool isDigit(char a);
+	size_t getFileSize(std::ifstream& file);
+	int findEntityIndexInFile(const MyString& filePath, const MyString& usernameToFind);
 }
